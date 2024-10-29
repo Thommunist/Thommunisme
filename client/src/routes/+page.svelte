e67f53a00f23d4fa3a7b0d5eddfb5d960775ddd0
Thom Sandbrink <83950281+Thommunist@users.noreply.github.com>
2024-10-29 15:48:24 +0100

    Update +page.svelte

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,9 +2,11 @@
 	import Portfolio from '$lib/topics/Portfolio.svelte';
 	import Welcome from '$lib/topics/Welcome.svelte';
 </script>
-
+<!--
 <span>
 	<Welcome />
 
 	<Portfolio />
 </span>
+-->
+
