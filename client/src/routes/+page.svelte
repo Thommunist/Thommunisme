746dcc1d11d9d9775b9c755dfa75fac243d60e3c
Thom Sandbrink <83950281+Thommunist@users.noreply.github.com>
2024-11-10 14:52:27 +0100

    Update +page.svelte

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import Portfolio from '$lib/topics/Portfolio.svelte';
 	import Welcome from '$lib/topics/Welcome.svelte';
 </script>
+
+Hier word gebouwd aan een portfolio website. 
+Coming Soon!
+
 <!--
 <span>
 	<Welcome />
