d22850516853a6a9cb9d54c3a9e7f26bd4394cd6
Thommunist <1thom@hotmail.nl>
2025-01-10 11:32:02 +0100

    Fixed issues

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '$lib/app.css';
+	import Footer from '$lib/footer/Footer.svelte';
+</script>
+
+<main
+	class="bg-backgroundLight dark:bg-backgroundDark flex min-h-screen w-full justify-center text-black dark:text-white"
+>
+	<div
+		class="bg-backgroundLight flex w-[98%] max-w-[1800px] flex-col content-center items-center justify-center"
+	>
+		<Header />
+		<div class="flex w-[80%] flex-col">
+			<slot />
+		</div>
+		<Footer />
+	</div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,8 @@
 	import Welcome from '$lib/topics/Welcome.svelte';
 </script>
 
-Hier word gebouwd aan een portfolio website. 
-Coming Soon!
-
-<!--
 <span>
 	<Welcome />
 
 	<Portfolio />
 </span>
--->
-
